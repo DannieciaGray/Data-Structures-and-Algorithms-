8d2fd540f532c9065e02fb516643c3f880f6e271
dannieciagray <dannieciagray@gmail.com>
2025-04-21 22:20:19 -0400

    Create Sorting and Searching.py

---
+++ Data Structures/Sorting and Searching.py
@@ -0,0 +1,89 @@
+def sequential_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i 
+        
+        return -1 
+
+def binary_search(arr,target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right :
+        middle = (left + right)//2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            right = middle - 1
+
+        else:
+            left = middle + 1
+
+    return -1 
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr 
+
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    less = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
+
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
